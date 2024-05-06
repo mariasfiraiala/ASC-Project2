bc0d57fe085bf4677e036b7b33232742bd59b8bf
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2024-05-06 21:31:58 +0300

    Add skel
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

---
+++ gpu_miner/src/gpu_miner.cu
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "../include/utils.cuh"
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+// TODO: Implement function to search for all nonces from 1 through MAX_NONCE (inclusive) using CUDA Threads
+__global__ void findNonce() {
+}
+
+int main(int argc, char **argv) {
+	BYTE hashed_tx1[SHA256_HASH_SIZE], hashed_tx2[SHA256_HASH_SIZE], hashed_tx3[SHA256_HASH_SIZE], hashed_tx4[SHA256_HASH_SIZE],
+			tx12[SHA256_HASH_SIZE * 2], tx34[SHA256_HASH_SIZE * 2], hashed_tx12[SHA256_HASH_SIZE], hashed_tx34[SHA256_HASH_SIZE],
+			tx1234[SHA256_HASH_SIZE * 2], top_hash[SHA256_HASH_SIZE], block_content[BLOCK_SIZE];
+	BYTE block_hash[SHA256_HASH_SIZE] = "0000000000000000000000000000000000000000000000000000000000000000"; // TODO: Update
+	uint64_t nonce = 0; // TODO: Update
+	size_t current_length;
+
+	// Top hash
+	apply_sha256(tx1, strlen((const char*)tx1), hashed_tx1, 1);
+	apply_sha256(tx2, strlen((const char*)tx2), hashed_tx2, 1);
+	apply_sha256(tx3, strlen((const char*)tx3), hashed_tx3, 1);
+	apply_sha256(tx4, strlen((const char*)tx4), hashed_tx4, 1);
+	strcpy((char *)tx12, (const char *)hashed_tx1);
+	strcat((char *)tx12, (const char *)hashed_tx2);
+	apply_sha256(tx12, strlen((const char*)tx12), hashed_tx12, 1);
+	strcpy((char *)tx34, (const char *)hashed_tx3);
+	strcat((char *)tx34, (const char *)hashed_tx4);
+	apply_sha256(tx34, strlen((const char*)tx34), hashed_tx34, 1);
+	strcpy((char *)tx1234, (const char *)hashed_tx12);
+	strcat((char *)tx1234, (const char *)hashed_tx34);
+	apply_sha256(tx1234, strlen((const char*)tx34), top_hash, 1);
+
+	// prev_block_hash + top_hash
+	strcpy((char*)block_content, (const char*)prev_block_hash);
+	strcat((char*)block_content, (const char*)top_hash);
+	current_length = strlen((char*) block_content);
+
+	cudaEvent_t start, stop;
+	startTiming(&start, &stop);
+
+	findNonce<<<1, 1>>>();
+
+	float seconds = stopTiming(&start, &stop);
+	printResult(block_hash, nonce, seconds);
+
+	return 0;
+}
