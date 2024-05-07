35cfd90d86a6d924c5f2a4f8aaa985f54ca08ef0
Maria Sfiraiala <maria.sfiraiala@gmail.com>
2024-05-07 12:03:21 +0300

    Add final implementation
    Signed-off-by: Maria Sfiraiala <maria.sfiraiala@gmail.com>

--- gpu_miner/src/gpu_miner.cu
+++ gpu_miner/src/gpu_miner.cu
@@ -5,16 +5,46 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-// TODO: Implement function to search for all nonces from 1 through MAX_NONCE (inclusive) using CUDA Threads
-__global__ void findNonce() {
+__device__ int done = 0;
+
+__global__ void findNonce(char *block_content_final, int current_length, char *block_hash_final, char *difficulty, uint64_t *nonce) {
+	char nonce_string[NONCE_SIZE];
+	char block_hash[SHA256_HASH_SIZE];
+	char block_content[BLOCK_SIZE];
+	d_strcpy(block_content, block_content_final);
+
+	int global_index = threadIdx.x + blockDim.x * blockIdx.x;
+	int p = blockDim.x * gridDim.x;
+	int n = MAX_NONCE;
+
+	int start = global_index * (double)n / p;
+	int end = (global_index + 1) * (double)n / p;
+
+	for (int i = start; i < end; ++i) {
+		if (done)
+			break;
+
+		intToString(i, nonce_string);
+        d_strcpy(block_content + current_length, nonce_string);
+        apply_sha256((BYTE *)block_content, d_strlen(block_content), (BYTE *)block_hash, 1);
+
+        if (compare_hashes((BYTE *)block_hash, (BYTE *)difficulty) <= 0) {
+			if (!atomicCAS(&done, 0, 1)) {
+				d_strcpy(block_hash_final, block_hash);
+				*nonce = i;
+			}
+            break;
+        }
+
+	}
 }
 
 int main(int argc, char **argv) {
 	BYTE hashed_tx1[SHA256_HASH_SIZE], hashed_tx2[SHA256_HASH_SIZE], hashed_tx3[SHA256_HASH_SIZE], hashed_tx4[SHA256_HASH_SIZE],
 			tx12[SHA256_HASH_SIZE * 2], tx34[SHA256_HASH_SIZE * 2], hashed_tx12[SHA256_HASH_SIZE], hashed_tx34[SHA256_HASH_SIZE],
 			tx1234[SHA256_HASH_SIZE * 2], top_hash[SHA256_HASH_SIZE], block_content[BLOCK_SIZE];
-	BYTE block_hash[SHA256_HASH_SIZE] = "0000000000000000000000000000000000000000000000000000000000000000"; // TODO: Update
-	uint64_t nonce = 0; // TODO: Update
+	BYTE block_hash[SHA256_HASH_SIZE] = "0000000000000000000000000000000000000000000000000000000000000000";
+	uint64_t nonce = 0;
 	size_t current_length;
 
 	// Top hash
@@ -37,13 +67,31 @@ int main(int argc, char **argv) {
 	strcat((char*)block_content, (const char*)top_hash);
 	current_length = strlen((char*) block_content);
 
+	char *dev_block_content, *dev_block_hash, *dev_difficulty;
+	uint64_t *dev_nonce;
+
 	cudaEvent_t start, stop;
 	startTiming(&start, &stop);
+	
+	cudaMalloc((void **) &dev_block_content, BLOCK_SIZE);
+	cudaMalloc((void **) &dev_block_hash, SHA256_HASH_SIZE);
+	cudaMalloc((void **) &dev_difficulty, SHA256_HASH_SIZE);
+	cudaMalloc((void **) &dev_nonce, sizeof(*dev_nonce));
+	cudaMemcpy(dev_block_content, block_content, BLOCK_SIZE, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_difficulty, DIFFICULTY, SHA256_HASH_SIZE, cudaMemcpyHostToDevice);
 
-	findNonce<<<1, 1>>>();
+	findNonce<<<50, 256>>>(dev_block_content, current_length, dev_block_hash, dev_difficulty, dev_nonce);
+
+	cudaMemcpy(block_hash, dev_block_hash, SHA256_HASH_SIZE, cudaMemcpyDeviceToHost);
+	cudaMemcpy((void *)&nonce, dev_nonce, sizeof(*dev_nonce), cudaMemcpyDeviceToHost);
 
 	float seconds = stopTiming(&start, &stop);
 	printResult(block_hash, nonce, seconds);
+
+	cudaFree(dev_block_content);
+	cudaFree(dev_block_hash);
+	cudaFree(dev_difficulty);
+	cudaFree(dev_nonce);
 
 	return 0;
 }
